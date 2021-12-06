3e7790b4c4ded1f452f40d07a157d58a3c64b5ed
TuanTQ <tquoctua97@gmail.com>
2021-12-06 23:53:37 +0700

    change method update todo

--- router.js
+++ router.js
@@ -4,11 +4,11 @@ const cors = require('cors')
 
 apiRouter.use(cors())
 
-apiRouter.get('/', (req, res) => res.json('Test running'))
+apiRouter.get('/', (req, res) => res.json('Test server successfully. Checkout code here: https://github.com/tquoctuan97/api-todo'))
 
 apiRouter.get('/todos', todoController.apiGet)
 apiRouter.post('/todos', todoController.apiCreate)
-apiRouter.put('/todos/:id', todoController.apiUpdate)
+apiRouter.post('/todos/:id', todoController.apiUpdate)
 apiRouter.delete('/todos/:id', todoController.apiDelete)
 
 module.exports = apiRouter
